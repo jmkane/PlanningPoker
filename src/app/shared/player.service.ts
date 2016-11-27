17a00870488bd11fc93f016a54fbf2035c5f2897
jmkane <jkane76@outlook.com>
2016-11-26 20:21:22 -0700

    Add files via upload

--- src/app/shared/player.service.ts
+++ src/app/shared/player.service.ts
@@ -1,54 +1,21 @@
-import {Injectable} from '@angular/core';
-const MongoClient = require('mongodb').MongoClient;
 import 'rxjs/add/operator/toPromise';
+const ADD = require('../../addPlayer');
+const GET = require('../../getPlayer');
+const UPDATE = require('../../updatePlayer');
 
 import Player from './player';
 
-
-@Injectable()
 export default class PlayerService {
 
-  player: any;
+  player: Player;
 
-  getPlayer(player): Promise<any[]> {
-    return MongoClient.then(db => {
-      const query = {player};
-      return db.collection('player')
-        .find(query)
-        .toArray();
-    });
+  GET(player) {
   };
 
-  updatePlayer(player, _id): Promise<any> {
+  UPDATE(player, _id) {
+  };
 
-    const copyPlayer = JSON.parse(JSON.stringify(player))
-
-    if(copyPlayer._id){
-      delete copyPlayer._id;
-    }
-    if(copyPlayer._id) {
-      delete copyPlayer._id;
-    }
-    return MongoClient.then(db => {
-      const collection = db.collection('player');
-      return collection.findOneAndUpdate
-    ( {copyPlayer})
-      .toPromise()
-      .then(response => {
-        return response.json()
-      })
-  },
- };
-
-  addPlayer(player: Player) {
-      return MongoClient.then(db => {
-        const collection = db.collection('player');
-        return collection.insertOne( JSON.parse(JSON.stringify(player)))
-      .toPromise()
-      .then(response => {
-        return response.json()
-      })
-  },
-};
+  ADD(player: Player) {
+  };
 
 }
